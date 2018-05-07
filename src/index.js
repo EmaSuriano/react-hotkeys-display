bf30687a6fa3c4a6e6267cba4188b7e95ddd76ad
Emanuel Suriano <emanuel.suriano@gmail.com>
2018-05-06 21:14:23 -0300

    Add missing files from root dir

--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
-/**
- * @class ExampleComponent
- */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-
-import styles from './styles.css'
+import styles from './styles.css';
 
 export default class ExampleComponent extends Component {
   static propTypes = {
-    text: PropTypes.string
-  }
+    text: PropTypes.string,
+  };
 
   render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+    const { text } = this.props;
+    console.log('asdasdss');
+    return <div className={styles.test}>Example Component pedro: {text}</div>;
   }
 }
